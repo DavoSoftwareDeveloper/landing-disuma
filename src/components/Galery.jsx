71b4203ab4098099a0f9dc487ea65eae9884a433
DavoDev <davidlarrosa58@gmail.com>
2023-04-28 17:32:24 +0200

    galery section

--- src/components/Galery.jsx
+++ src/components/Galery.jsx
@@ -1,9 +1,40 @@
-
+import foto2 from "../assets/exterior2.jpeg"
 
 function Galery() {
   return (
-    <div>
-      Galery
+    <div className="galery">
+      <div className="galery-content">
+          <section className="section">
+            <div className="photo-container">
+              <div className="flex">
+                <img className="image" src={foto2} alt="galery-photo" />
+                <img className="image" src={foto2} alt="galery-photo" />
+              </div>
+              <div className="image-tall">
+                <img className="image2" src={foto2} alt="galery-photo" />
+              </div>
+            </div>
+            <div className="details-photo">
+              <h2>Hello</h2>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum pariatur amet consequuntur, officia tenetur consectetur esse ratione, culpa quidem, molestiae placeat aut! Placeat, deleniti molestias expedita cum est sapiente laudantium.</p>
+            </div>
+          </section>
+          <section className="section">
+            <div className="details-photo">
+            <h2>Hello2</h2>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum pariatur amet consequuntur, officia tenetur consectetur esse ratione, culpa quidem, molestiae placeat aut! Placeat, deleniti molestias expedita cum est sapiente laudantium.</p>
+            </div>
+            <div className="photo-container2">
+            <div className="flex">
+                <img className="image" src={foto2} alt="galery-photo" />
+                <img className="image" src={foto2} alt="galery-photo" />
+              </div>
+              <div className="image-tall">
+                <img className="image2" src={foto2} alt="galery-photo" />
+              </div>
+            </div>
+          </section>
+      </div>
     </div>
   )
 }
