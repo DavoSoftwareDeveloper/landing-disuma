9426fa346c78abebd13e580b4966aad66b4246ac
DavoDev <davidlarrosa58@gmail.com>
2023-04-28 11:24:40 +0200

    standard nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,9 +2,13 @@
 
 function Nav() {
   return (
-    <div>
-      Nav
-    </div>
+    <ul className="Navbar">
+        <li><a href="#about">About</a></li>
+        <li><a href="#skills">Skills</a></li>
+        <li><a href="#work">Work</a></li>
+        <li><a href="#projects">Projects</a></li>
+        <li><a href="#contact">Contact</a></li>
+    </ul>
   )
 }
 
