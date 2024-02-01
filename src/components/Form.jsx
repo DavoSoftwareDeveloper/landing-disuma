8d6e492ff6115b2f456531ec29e0aa8b59a827c8
DavoDev <davidlarrosa58@gmail.com>
2024-02-01 11:31:11 +0100

    improve Form component

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,33 +1,98 @@
 import '../styles/form.css'
-import { useState } from "react"
 import { ToastContainer, toast } from 'react-toastify';
+import { useForm } from "react-hook-form"
+
 
 
 function Form() {
-
-  const [ formData, setFormData ] = useState({
-    name: '',
-    email: '',
-    telefono: '',
-    peticion: ''
-})
-
-const {name, email, telefono, peticion } = formData
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+    reset,
+  } = useForm()
 
 
-const onChange = (e) => {
-    setFormData((prevState) =>({
-        ...prevState,
-        [e.target.name]: e.target.value
-    }))
-}
-const onSubmit = (e) => {
-    e.preventDefault()
-    toast.success("mensaje enviado!")
-    console.log(formData);
+  const onSubmit = (data) => {
+      console.log(data)
+      toast.success("mensaje enviado!")
+      reset()
+  }
 
 
-    }
+  console.log(watch("name")) // watch input value by passing the name of it
+
+
+//   const [ formData, setFormData ] = useState({
+//     name: null,
+//     email: null,
+//     telefono: null,
+//     peticion: null
+// })
+// const [error, setError] = useState({
+//     errorName: null,
+//     errorEmail: null,
+//     errorTelefono: null,
+//     errorPeticion: null
+// })
+
+// const {name, email, telefono, peticion } = formData
+
+
+// const onChange = (e) => {
+//     switch(e.target.name){
+//         case "name":
+//             if (e.target.value.length < 2 || e.target.value.length > 30) { 
+//                  setError(prev => ({...prev, errorName: "Introduce un nombre válido"}))}
+//             else { 
+//                error.errorName && setError(prev => ({...prev, errorName: null}))
+//             }
+//         break;
+//         case "email":
+//             if (e.target.value.length < 2 || e.target.value.length > 30) { 
+//                  setError(prev => ({...prev, errorName: "Introduce un email válido"}))}
+//             else { 
+//                error.errorName && setError(prev => ({...prev, errorName: null}))
+//             }
+//         break;
+//         case "telefono":
+//             if (e.target.value.length < 2 || e.target.value.length > 30) { 
+//                  setError(prev => ({...prev, errorName: "Introduce un telefono válido"}))}
+//             else { 
+//                error.errorName && setError(prev => ({...prev, errorName: null}))
+//             }
+//         break;
+//         case "peticion":
+//             if (e.target.value.length < 2 || e.target.value.length > 30) { 
+//                  setError(prev => ({...prev, errorName: "Introduce un mensaje válido"}))}
+//             else { 
+//                error.errorName && setError(prev => ({...prev, errorName: null}))
+//             }
+//         break;
+
+
+//     }
+//     setFormData((prevState) =>({
+//         ...prevState,
+//         [e.target.name]: e.target.value
+//     }))
+// }
+// const onSubmit = (e) => {
+//     e.preventDefault()
+//     const errorArray = Object.values(error)
+//     const anyError = errorArray.find(value => value != null)
+//     console.log(anyError)
+//     if(anyError) return
+
+//     const allData  = Object.values(formData)
+//     const allDataFilled = allData.find(value => value === null)
+//     console.log(allDataFilled)
+//     if(allDataFilled === null) return
+
+//     toast.success("mensaje enviado!")
+//     console.log(formData);
+
     return ( 
       <>
       <section className='heading'>
@@ -39,30 +104,57 @@ const onSubmit = (e) => {
       </section>
       
       <section className='form'>
-      <form onSubmit={onSubmit}>
+      <form onSubmit={handleSubmit(onSubmit)}>
           <div className="form-group">
-              <label htmlFor="name">Nombre</label>
+              <label htmlFor="name">Nombre*</label>
               <input type='text' className='form-control' id='name' 
-              name='name' value={name} placeholder='Introduce tu nombre'
-              onChange={onChange} />
+              name='name' placeholder='Introduce tu nombre'
+              {...register("name", {required: {
+                value: true,
+                message: "Nombre es requerido"
+              }, maxLength:{
+                value: 20,
+                message:"El nombre debe tener menos de 20 caracteres"
+              }, minLength:{
+                value: 2,
+                message: "El nombre debe tener más de 2 caracteres"
+              } })}
+              />
+                     { errors.name && <span style={{color:"red"}}>{errors.name.message}</span>}
           </div>
+          
           <div className="form-group">
-              <label htmlFor="name">E-mail</label>
+              <label htmlFor="email">E-mail*</label>
               <input type='email' className='form-control' id='email' 
-              name='email' value={email} placeholder='Introduce tu email'
-              onChange={onChange} />
+              name='email' placeholder='Introduce tu email'
+              {...register("email", {required: {
+                value: true,
+                message: "El email es obligatorio"
+              }, pattern:{
+                value: '^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$',
+                message: "El email no es válido"
+              }})}
+               />
+               { errors.email && <span style={{color:"red"}}>{errors.email.message}</span>}
           </div>
           <div className="form-group">
-              <label htmlFor="name">Teléfono</label>
-              <input type='text' className='form-control' id='telefono' 
-              name='telefono' value={telefono} placeholder='Introduce tu teléfono'
-              onChange={onChange} />
+              <label htmlFor="telefono">Teléfono</label>
+              <input type='number' className='form-control' id='telefono' 
+              name='telefono' placeholder='Introduce tu teléfono'
+              {...register("telefono")}
+              />
+              { errors.telefono && <span style={{color:"red"}}>{errors.telefono.message}</span>}
           </div>
           <div className="form-group">
-              <label htmlFor="name">En qué podemos ayudarte?</label>
+              <label htmlFor="peticion">En qué podemos ayudarte?*</label>
               <textarea type='text' className='form-control2' id='peticion' 
-              name='peticion' value={peticion} placeholder='Me gustaría pedir presupuesto sobre...'
-              onChange={onChange} />
+              name='peticion'  placeholder='Me gustaría pedir presupuesto sobre...'
+              {...register("peticion", {required: {
+                value: true,
+                message: "El mensaje es obligatorio"
+              }})}
+               />
+               { errors.peticion && <span style={{color:"red"}}>{errors.peticion.message}</span>}
           </div>
           <div className='form-group'>
               <button type='submit' className='btn'>Enviar</button>
